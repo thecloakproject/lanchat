ac71a916f13fb1d8e5eda14099aa216b68c66c30
Steve Phillips <elimisteve@gmail.com>
2012-11-27 02:13:19 -0800

    ...not sure this accomplished anything...

--- lanchat.go
+++ lanchat.go
@@ -125,21 +125,30 @@ func TCPServer(listenIPandPort string, maxConns int, handler func(net.Conn)) {
 
 	activeConns := make(chan int, maxConns)
 
-	// Accept new TCP connections forever
 	for {
+		// Every time someone connects and the number of active connections
+		// <= maxConns, handle the connection
+
+		activeConns <- 1
+		if DEBUG {
+			log.Printf("Added 1 to semaphore. Accepting connections...\n")
+		}
+
+		// Accept new connections
 		conn, err := listener.Accept()
+		if DEBUG {
+			log.Printf("New connection: %s\n", conn.RemoteAddr())
+		}
 		if err != nil {
 			log.Printf("RemoteTCPServer: Error accepting TCP traffic: %v", err)
+			<-activeConns
+			if DEBUG {
+				log.Printf("activeConns drained by 1\n")
+			}
 			continue
 		}
-		// Every time someone connects, spawn goroutine that handles the
-		// connection once the number of connections <= maxConns
-		go func() {
-			activeConns <- 1
-			if DEBUG {
-				log.Printf("Added 1 to semaphore. Calling handler\n")
-			}
 
+		go func() {
 			handler(conn)
 			if DEBUG {
 				log.Printf("handler for %s returned\n", conn.RemoteAddr())
