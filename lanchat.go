fffe71b33008bccd43c83cba32d636f566d8350b
Steve Phillips <elimisteve@gmail.com>
2013-01-04 21:29:45 -0800

    Max length of encrypted _and_ plaintext messages are now 1MB, were 8192 bytes (arbitrarily)

--- lanchat.go
+++ lanchat.go
@@ -22,7 +22,7 @@ import (
 const (
 	// TODO: Does this really do anything? Seems like the block size
 	// is determining how messages get carved up...
-	MAX_MESSAGE_SIZE = 8192
+	MAX_MESSAGE_SIZE = 1e6 // 1MB
 )
 
 var (
